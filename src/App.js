3a147a77acc82e2826b218265af484456e0c8cb5
jvii <jani.viitala1@gmail.com>
2022-03-20 12:59:33 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import Form from './Form';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Squash Intro</h1>
+      <p>
+        Nääs Squash järjestää lajista kiinnostuneille Squash Intro -tapahtumia Tampereen
+        Tenniskeskuksella.
+      </p>
+      <ul>
+        <li>&#x2705; Opastusta lajin pariin vasta-alkajille</li>
+        <li>&#x1F4A1; Tekniikkaa ja taktiikkaa kokeneemmillekin pelureille</li>
+        <li>&#x1F64C; Uusia pelikavereita</li>
+      </ul>
+      <p>
+        Jätä sähköpostiosoitteesi alla olevaan kenttään ja ilmoitamme sinulle, kun seuraava
+        tapahtuma järjestetään.
+      </p>
+      <Form />
     </div>
   );
 }
---
+++ src/Form.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Form() {
+  return (
+    <form name="contact" method="post">
+      <p>
+        <label htmlFor="email">Email</label> <br />
+        <input type="email" id="email" name="email" required />
+      </p>
+      <p>
+        <label htmlFor="message">Message</label> <br />
+        <textarea id="message" name="message" required></textarea>
+      </p>
+      <p>
+        <input type="submit" value="Lähetä" />
+      </p>
+    </form>
+  );
+}
